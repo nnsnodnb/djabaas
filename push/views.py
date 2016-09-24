89512a1492c3e5f7ef6455f8362145ba01cb7e67
nnsnodnb <ahr63_gej@me.com>
2016-09-24 18:32:51 +0900

    Update device token list viewer

--- push/views.py
+++ push/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseForbidden
 from push.models import DeviceTokenModel
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
@@ -12,11 +12,10 @@ def index(request):
     return render_to_response('push/top.html',
                              {'device_tokens': device_tokens},
                              context_instance = RequestContext(request))
-    # return HttpResponse('Hello Django')
 
 @csrf_exempt
 def device_token_register(request):
-    if request.method == 'POST' and request.META['HTTP_USER_AGENT'] == 'iOS/nnsnodnb-mBaaS-Service':
+    if request.method == 'POST':# and request.META['HTTP_USER_AGENT'] == 'iOS/nnsnodnb-mBaaS-Service':
         receive_json = json.loads(request.body)
         insert_data = DeviceTokenModel(os_version = receive_json['os_version'],
                                        device_token = receive_json['device_token'])
@@ -27,4 +26,5 @@ def device_token_register(request):
         response_data['message'] = '"' + receive_json['device_token'] + '" is registered'
         return HttpResponse(json.dumps(response_data), content_type="application/json")
     else:
-        return HttpResponse('Not allow reuqest')
+        #return HttpResponse('Not allow reuqest')
+        return HttpResponseForbidden()
