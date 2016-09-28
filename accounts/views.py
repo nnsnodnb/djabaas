93f6a19f5dadba3ed1b59da88f6275552a569af7
nnsnodnb <ahr63_gej@me.com>
2016-09-29 01:25:12 +0900

    Tentatively register & forget page

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,25 @@
+from django.contrib.auth.models import User
+from django.core.mail import EmailMessage
 from django.http import HttpResponse
 from django.shortcuts import render, render_to_response
 from django.template.context_processors import csrf
 
 def register(request):
-    c = {}
-    c.update(csrf(request))
-    return render_to_response('accounts/register.html', c)
+    if request.method == 'POST':
+        return HttpResponse('hello')
+    else:
+        c = {}
+        c.update(csrf(request))
+        return render_to_response('accounts/register.html', c)
 
 def forget(request):
-    c = {}
-    c.update(csrf(request))
-    return render_to_response('accounts/forget.html', c)
+    if request.method == 'POST':
+        try:
+            EmailMessage(u'title', u'body', to = ['hoge@example.com']).send()
+            return HttpResponse('Send your register email')
+        except Exception as e:
+            raise
+    else:
+        c = {}
+        c.update(csrf(request))
+        return render_to_response('accounts/forget.html', c)
