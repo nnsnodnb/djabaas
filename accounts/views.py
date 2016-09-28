8db4a465aaf2d39cd8ad3a74863633d7b45f9fca
nnsnodnb <ahr63_gej@me.com>
2016-09-29 01:04:31 +0900

    Create register & forget site models

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, render_to_response
+from django.template.context_processors import csrf
 
-# Create your views here.
+def register(request):
+    c = {}
+    c.update(csrf(request))
+    return render_to_response('accounts/register.html', c)
+
+def forget(request):
+    c = {}
+    c.update(csrf(request))
+    return render_to_response('accounts/forget.html', c)
