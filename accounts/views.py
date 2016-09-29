f23e2215392e827e04f98e233bd7e07bfab4896e
nnsnodnb <ahr63_gej@me.com>
2016-09-29 23:26:20 +0900

    Create user session

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,28 @@
 from django.contrib.auth.models import User
 from django.core.mail import EmailMessage
 from django.http import HttpResponse
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render, render_to_response, redirect
 from django.template.context_processors import csrf
+import urllib
 
 def register(request):
     if request.method == 'POST':
-        return HttpResponse('hello')
+        arrays = request.body.split('&')
+        query = {}
+        for item in arrays:
+            tmp_arrays = item.split('=')
+            query[tmp_arrays[0]] = tmp_arrays[1]
+
+        if query['password'] == query['password_confirm']:
+            user = User(username = query['username'],
+                        email = urllib.unquote(query['email']),
+                        password = query['password'])
+            user.save()
+            return render_to_response('/accounts/login/next=/')
+        else:
+            c = {}
+            c.update(csrf(request))
+            return render_to_response('accounts/register.html', c)
     else:
         c = {}
         c.update(csrf(request))
