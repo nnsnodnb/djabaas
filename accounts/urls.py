3f9a8d2c1e8f8701af7f44d4e3a0388c4c8202f1
nnsnodnb <ahr63_gej@me.com>
2016-10-01 03:39:05 +0900

    [WIP] change user's password

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^login/$', login, {'template_name': 'accounts/login.html'}, name = 'login'),
     url(r'^logout/$', logout, {'template_name': 'accounts/logout.html'}, name = 'logout'),
     url(r'^register/$', views.register, name = 'register'),
-    url(r'^forget/$', views.forget, name = 'forget')
+    url(r'^forget/$', views.forget, name = 'forget'),
+    url(r'^change_pass', views.change_pass, name = 'change_pass'),
 ]
