73cb9dd2dc1f9fc4de2632910420e5bfc4512897
nnsnodnb <ahr63_gej@me.com>
2016-10-03 23:51:53 +0900

    Update push application DB models

--- push/models.py
+++ push/models.py
@@ -5,28 +5,28 @@ from django.db import models
 
 class DeviceTokenModel(models.Model):
     os_version = models.FloatField()
-    device_token = models.CharField(max_length = 100, default = None)
+    device_token = models.CharField(max_length = 100, blank = True)
     register_datetime = models.DateTimeField(default = datetime.now)
     update_datetime = models.DateTimeField(default = datetime.now)
     username = models.CharField(max_length = 50)
 
 class NotificationModel(models.Model):
-    title = models.CharField(max_length = 200, default = None)
-    message = models.CharField(max_length = 500, default = None)
+    title = models.CharField(max_length = 200, blank = True)
+    message = models.CharField(max_length = 500, blank = True)
     os_version = models.FloatField()
-    sound = models.CharField(max_length = 30, default = None)
-    badge = models.IntegerField(default = None)
-    url = models.CharField(max_length = 200, default = None)
-    json = models.CharField(max_length = 150, default = None)
+    sound = models.CharField(max_length = 30, blank = True)
+    badge = models.IntegerField(blank = True)
+    url = models.CharField(max_length = 200, blank = True)
+    json = models.CharField(max_length = 150, blank = True)
     content_available = models.BooleanField(default = False)
     is_production = models.BooleanField(default = False)
     register_date = models.DateTimeField(default = datetime.now)
     username = models.CharField(max_length = 50)
 
 class DevelopFileModel(models.Model):
-    upload_username = models.CharField(max_length = 50, default = None)
-    development_file_name = models.CharField(max_length = 100, default = None)
+    upload_username = models.CharField(max_length = 50, blank = True)
+    development_file_name = models.CharField(max_length = 100, blank = True)
 
 class ProductFileModel(models.Model):
-    upload_username = models.CharField(max_length = 50, default = None)
-    production_file_name = models.CharField(max_length = 100, default = None)
+    upload_username = models.CharField(max_length = 50, blank = True)
+    production_file_name = models.CharField(max_length = 100, blank = True)
