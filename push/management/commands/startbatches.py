6bd28dc0e80dcf8513a2711a984aa9d9a1ee88fe
nnsnodnb <ahr63_gej@me.com>
2016-10-08 01:49:09 +0900

    Batch execute 'python /path/to/manage.py startbatches'

--- push/management/commands/startbatches.py
+++ push/management/commands/startbatches.py
@@ -1,8 +1,11 @@
 # coding=utf-8
 
+from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 from push.models import DeviceTokenModel, NotificationModel
 from datetime import datetime
+
+import push_notification
 
 class Command(BaseCommand):
     def __init__(self, *args, **kwargs):
@@ -10,4 +13,16 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         now = '{0:%Y/%m/%d %H:%M}'.format(datetime.now())
-        notification = NotificationModel.objects.filter(execute_datetime = now)
+        notifications = NotificationModel.objects.filter(execute_datetime = now)
+
+        for notification in notifications:
+            device_tokens = DeviceTokenModel.objects.filter(os_version__gte = notification.os_version,
+                                                            username = notification.username)
+            self.prepare_push_notification(notification, device_tokens)
+
+    def prepare_push_notification(self, notification, device_tokens):
+        device_token_lists = []
+        for item in device_tokens:
+            device_token_lists.append(item.device_token)
+
+        push_notification.execute(device_token_lists, notification)
