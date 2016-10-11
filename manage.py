8a724ab2640fc9b42ec8ac855cb731590f864b72
nnsnodnb <oka.03_05@me.com>
2016-10-11 23:33:09 +0900

    Remove static

--- manage.py
+++ manage.py
@@ -6,9 +6,6 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mbaas.settings")
 
-    import pymysql
-    pymysql.install_as_MySQLdb()
-
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
