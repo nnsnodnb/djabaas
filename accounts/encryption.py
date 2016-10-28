6661ddb4b8bb7f57eeb6d2b37375cf15425e8c7a
nnsnodnb <ahr63_gej@me.com>
2016-10-29 02:00:16 +0900

    Modify encryption

--- accounts/encryption.py
+++ accounts/encryption.py
@@ -1,15 +1,21 @@
 # coding=utf-8
 
-from Crypto.Hash import SHA256
-from Crypto import Random
+from simple_aes_cipher import AESCipher, generate_secret_key
+import os
 
 def execute_encryption(is_encryption, input_word):
+    pass_phrase = os.environ.get('ENCRYPT_PASS_PHARSE')
+    secret_key = generate_secret_key(pass_phrase)
+
+    # Generate cipher
+    cipher = AESCipher(secret_key)
+
     executed_word = ''
     if is_encryption:
-        hash_sha256 = SHA256.new()
-        hash_sha256.update(input_word)
-        executed_word = hash_sha256.hexdigest()
+        # Encryption
+        executed_word = cipher.encrypt(input_word)
     else:
-        executed_word = private_key.decrypt(input_word)
+        # Dencryption
+        executed_word = cipher.decrypt(input_word)
 
     return executed_word
