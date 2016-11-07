ff3997a571a6f1d7c0d9bd50a624dd0022ae7d6f
nnsnodnb <ahr63_gej@me.com>
2016-11-08 00:47:27 +0900

    Add Test prepare

---
+++ mbaas/settings_jenkins.py
@@ -0,0 +1,19 @@
+from mbaas.settings import *
+
+INSTALLED_APPS += ('django_nose',)
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+NOSE_ARGS = [
+    '--cover-erase',
+    '--with-coverage',
+    '--cover-html',
+    '--cover-package=accounts,push',
+]
+
+DATABASES = {
+    'default': {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': os.path.join(BASE_DIR, 'test.db'),
+    }
+}
