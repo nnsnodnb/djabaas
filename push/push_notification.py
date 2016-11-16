98c2fd639525b519a6306111073cea93d33376d7
nnsnodnb <ahr63_gej@me.com>
2016-11-16 22:33:22 +0900

    #26 Modify Notification status default

--- push/push_notification.py
+++ push/push_notification.py
@@ -41,6 +41,4 @@ def execute(device_token_lists, notification):
 
     apns.gateway_server.send_notification_multiple(frame)
 
-    notification.is_sent = True
-    notification.status = 1
     notification.save()
